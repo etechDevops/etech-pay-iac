4a8f6676aefda764d5227348eac62e634f3bc4bf
Etech consulting <99599657+etechDevops@users.noreply.github.com>
2023-03-06 21:35:36 -0500

    Create main.tf

---
+++ etech-pay/transaction/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "server" {
+  source        = "git@github.com/etechDevops/etech-base-iac/tree/main/etech-automodules//modules/ref?=vpc"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  num           = var.num
+}
